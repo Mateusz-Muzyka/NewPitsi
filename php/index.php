e3fe91852bb77d702b155063879cb6fef6dc8cf6
Mieciek <mateusz.muzyka@katowice.zdz.katowice.pl>
2025-01-27 23:35:34 +0100

    naprawioni wszystko co sie dało
    wyświetla obrazy

--- php/index.php
+++ php/index.php
@@ -4,6 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        img{
+            width: 100px;
+            height: 100px;
+            border: 1px black solid;
+            margin-left: 5px;
+        }
+    </style>
 </head>
 <body>
     <p>działa</p>
@@ -16,18 +24,22 @@
         // preg_match('/<img[^>]*src=[\'"]([^\'"]+)[\'"][^>]*>/i', $data, $matches);
         // $img = $matches[1];
         $qqq = explode("<img",$data);
+        // for($i = 0; $i<count($qqq);$i++){
+        //     echo "<textarea>";
+        //     $x = strpos($qqq[$i], 'src="');
+        //     $y = strpos($qqq[$i], '"',$x+5);
+        //     $z = $y+1 - $x;
+        //     // echo $z;
+        //     echo substr($qqq[$i],$x,$z);
+        //     echo "</textarea>";
+            
+        // }
         for($i = 0; $i<count($qqq);$i++){
-            echo "<textarea>";
             $x = strpos($qqq[$i], 'src="');
             $y = strpos($qqq[$i], '"',$x+5);
-            echo "x=".$x;
-            echo "y=".$y;
-            echo "<br>";
-            $z = $y - $x;
-            // echo $z;
-            echo substr($qqq[$i],$x,$z);
-            echo "</textarea>";
-            
+            $z = $y+1 - $x;
+            $q = substr($qqq[$i],$x,$z);
+            echo '<img '.$q.'>';
         }
 
         
