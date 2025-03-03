02da8b71f46e73ceb22cd5cbc029ce3093d17bdb
Mieciek <131866876+Mateusz-Muzyka@users.noreply.github.com>
2025-03-03 09:33:18 +0100

    Add files via upload

---
+++ php/php/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>działa</p>
+    <?php 
+        $pytania = [];
+        $odpowiedzi = [];
+
+
+        $data = file_get_contents('quiz.htm');
+        
+        // preg_match('/<title>([^<]+)<\/title>/i', $data, $titles);
+        // $title = $titles[1];
+    
+        // preg_match('/<img[^>]*src=[\'"]([^\'"]+)[\'"][^>]*>/i', $data, $matches);
+        // $img = $matches[1];
+        $qqq = explode('class="trescE">',$data);
+        for($i = 0; $i<count($qqq);$i++){
+            
+            $x = strpos($qqq[$i], '.');
+            $y = strpos($qqq[$i], '</div');
+            $x = $x+2;
+            // echo "x=".$x;
+            // echo "y=".$y;
+            if($i > 0)
+            {
+                echo "<textarea>";
+                echo substr($qqq[$i],$x,$y);
+                echo "</textarea>";
+                $pytania[] = substr($qqq[$i],$x,$y);
+            }
+            
+        }
+        echo "<hr>";
+        echo "<hr>";
+        $qqq2 = explode('class="odpgood">',$data);
+        $qqq3 = explode('<div id="odpa',$data);
+        for($i = 0; $i<count($qqq2);$i++){
+            
+            $x = strpos($qqq2[$i], '.');
+            $y = strpos($qqq2[$i], '</div>');
+            $x = $x + 2;
+            $y = $y;
+            $z = $y - $x;
+
+            if($i > 0)
+         {
+                // echo "<textarea>";
+                // echo substr($qqq2[$i],$x,$z);
+                // echo "</textarea>";
+                $odpowiedzi[] = substr($qqq2[$i],$x,$z);
+                
+                // echo substr($qqq2[$i],$x,$z);
+  
+                $xT = strpos($qqq3[$i], 'A.');
+                $yT = strpos($qqq3[$i],'id="odpb');
+                $zT = $yT - $xT;
+                
+                $xT2 = strpos($qqq3[$i], 'B.');
+                $yT2 = strpos($qqq3[$i],'id="odpc');
+                $zT2 = $yT2 - $xT2;
+                
+                
+                $xT3 = strpos($qqq3[$i], 'C.');
+                $yT3 = strpos($qqq3[$i],'id="odpd');
+                // $q3 = substr($qqq3[$i],$xT+3,$zT);
+
+                $zT3 = $yT3 - $xT3;
+                
+                $xT4 = strpos($qqq3[$i], 'D.');
+                $yT4 = strpos($qqq3[$i],' class="sep">');
+                $zT4 = $yT4 - $xT4;
+echo '<textarea>';
+echo '<li id="good">'.substr($qqq2[$i],$x,$z). '<li>';
+echo '<li>'.substr($qqq3[$i],$xT+3,$zT).'</li>';
+echo '</textarea>';
+echo $pytania[$i-1];
+                echo '<section id="qqq">';
+                    echo '<ol type="A">';
+
+                        echo '<li>'.substr($qqq3[$i],$xT+3,$zT).'</li>';
+                        echo '<li>'.substr($qqq3[$i],$xT2+3,$zT2).'</li>';
+                        echo '<li>'.substr($qqq3[$i],$xT3+3,$zT3).'</li>';
+                        echo '<li>'.substr($qqq3[$i],$xT4+3,$zT4).'</li>';
+                
+                echo '</ol>';
+                echo '</section>';
+            }
+        }
+        echo "<hr>";
+        echo "<hr>";
+
+        // for($i = 0; $i<count($pytania);$i++){
+        //     echo $pytania[$i];
+        //     echo "<br>";
+        //     echo "<br>";
+        //     echo $odpowiedzi[$i];
+        //     echo "<br>";
+        //     echo "<hr>";
+        // }
+    ?>
+</body>
+</html>
+
+
+
